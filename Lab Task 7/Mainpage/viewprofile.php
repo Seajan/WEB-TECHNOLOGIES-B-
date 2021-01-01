d95cc816197e7bab1688f7cd5f960d36c548cb16
Seajan Shariar <52612134+Seajan@users.noreply.github.com>
2021-01-01 16:09:10 +0600

    Add files via upload

---
+++ Lab Task 7/Mainpage/viewprofile.php
@@ -0,0 +1,16 @@
+<?php
+include "../mainpage/common.inc.php";
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div align="center">
+        <h1>Hello</h1>
+    </div>
+    
+</body>
+</html>
